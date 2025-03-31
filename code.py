745f617ec783a1b8846b4e109b2ab6855482ba46
ThatGuyRuchir <150929311+Ruchir-Kafle@users.noreply.github.com>
2025-03-31 09:44:53 -0400

    added main file

---
+++ code.py
@@ -0,0 +1,8 @@
+import time
+from random import randint
+import board
+from adafruit_clue import clue
+from adafruit_debouncer import Debouncer
+import displayio
+from adafruit_bitmap_font import bitmap_font
+from adafruit_display_text import label
